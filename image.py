6586f3bb43ebc5bfecd31383a1a59b1c6327cf85
Nate Card <ncard24@gmail.com>
2024-02-14 13:18:19 -0500

    Update alternate models and add custom VAE support for MPS

--- image.py
+++ image.py
@@ -10,16 +10,19 @@ import torch
 vae = AutoencoderKL.from_pretrained(
     "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
 )
-
 # Directory to save the images
 test_dir = os.path.join("test_images")
 # Create the directory if it doesn't exist
 os.makedirs(test_dir, exist_ok=True)
 
 pipeline = DiffusionPipeline.from_pretrained(
-    "stabilityai/sdxl-turbo",
+    # Uncomment to use the SDXL Turbo model
+    # "stabilityai/sdxl-turbo",
+    # Comment out to use the SDXL Turbo model
+    "segmind/SSD-1B",
     torch_dtype=torch.float16,
     variant="fp16",
+    vae=vae,
     use_safetensors=True,
 ).to("mps")
 helper = DeepCacheSDHelper(pipe=pipeline)
@@ -28,8 +31,12 @@ helper.set_params(
     cache_branch_id=0,
 )
 helper.enable()
-# Does not work with SDXL Turbo and MPS
-# pipeline.enable_attention_slicing()
+# Torch._dynamo issues with MPS? (not sure)
+# pipeline.unet = torch.compile(pipeline.unet, mode="reduce-overhead", fullgraph=True)
+# Does not work with custom VAE
+# pipeline.enable_attention_slicing("max")
+
+
 # Refiner model for XL pipeline (better quality & optional, but slower)
 refiner = DiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-refiner-1.0",
@@ -40,9 +47,7 @@ refiner = DiffusionPipeline.from_pretrained(
 ).to("mps")
 
 
-# pipeline.unet = torch.compile(pipeline.unet, mode="reduce-overhead", fullgraph=True)
-
-# Used to reduce GPU memory usage
+# Used to reduce GPU memory usage when using CUDA
 # pipeline.enable_sequential_cpu_offload()
 # Used to reduce memory overhead
 # pipeline.enable_attention_slicing("max")
@@ -50,22 +55,32 @@ refiner = DiffusionPipeline.from_pretrained(
 prompt = (
     input("Enter a prompt: ") or "A painting of an elephant in the style of Picasso."
 )
-# save_name = input("Save as: ") or "image.png"
-
+neg_prompt = (
+    input("Enter a negative prompt: ")
+    or "follow the prompted image as closely as possible."
+)
+# Number of inference steps
 n_steps = 50
-high_noise_frac = 0.7
+# For SDXL to denoise the image
+# high_noise_frac = 0.7
 
 image = pipeline(
     prompt=prompt,
-    num_inference_steps=n_steps,
-    output_type="latent",
+    negative_prompt=neg_prompt,
+    # If swapped to SDXL Turbo, use the following lines
+    # num_inference_steps=n_steps,
+    # output_type="image",
+    # change next line to images[0] if not using the refiner model
 ).images
 
+# Uncomment to use the refiner model for better quality
 image = refiner(
     prompt=prompt,
     num_inference_steps=n_steps,
     image=image,
 ).images[0]
+
+# Hugging Face utils API not working currently
 # Make a grid out of the images
 # images = image[0:]
 # image_grid = make_image_grid(images, rows=4, cols=4)
@@ -75,10 +90,3 @@ now = datetime.now()
 format_time = now.strftime("%m_%d_%h_%m_%s")
 # Save images to the directory with the current date and time
 image.save(f"{test_dir}/{now}_image.png")
-
-# if save_name.lower().endswith(".png"):
-#     save_name = save_name
-#     image.save(save_name)
-# else:
-#     save_name = save_name.lower().strip() + ".png"
-#     image.save(save_name)
